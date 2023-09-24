9a79e6bce3cac97e27bf3f184d3d1c18641ef124
Luan Rafael <luanraffaell@hotmail.com>
2023-09-24 04:19:31 -0300

    feat: criado produtoService

---
+++ Loja sapato/backend/src/main/java/com/lojasapato/lojasapato/domain/service/ProdutoService.java
@@ -0,0 +1,34 @@
+package com.lojasapato.lojasapato.domain.service;
+
+import com.lojasapato.lojasapato.domain.exception.EntidadeNaoEncontradaException;
+import com.lojasapato.lojasapato.domain.model.Produto;
+import com.lojasapato.lojasapato.infrastructure.repositories.ProdutoRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ProdutoService {
+    private final ProdutoRepository produtoRepository;
+
+    public List<Produto> listarProdutos(){
+        return this.produtoRepository.findAll();
+    }
+    public Produto buscarProdutoPorId(Long id){
+        return this.produtoRepository.findById(id)
+                .orElseThrow(() ->
+                        new EntidadeNaoEncontradaException("Não foi encontrado um produto com o id:"+id));
+    }
+    public List<Produto> listarProdutosPorNomeContendo(String nome){
+        return this.produtoRepository.findByNomeContaining(nome)
+                .orElseThrow(() ->
+                        new EntidadeNaoEncontradaException("Não foi encontrado produtos com o nome:"+nome));
+    }
+    @Transactional
+    public Produto cadastrarProduto(Produto produto){
+        return this.produtoRepository.save(produto);
+    }
+}
