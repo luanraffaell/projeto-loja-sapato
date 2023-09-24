00bcce1f39b3eab36a16071736f1086b679484cd
Luan Rafael <luanraffaell@hotmail.com>
2023-09-24 04:03:48 -0300

    feat: inserido metodo de busca por email em UsuarioRepository

--- Loja sapato/backend/src/main/java/com/lojasapato/lojasapato/infrastructure/repositories/UsuarioRepository.java
+++ Loja sapato/backend/src/main/java/com/lojasapato/lojasapato/infrastructure/repositories/UsuarioRepository.java
@@ -3,5 +3,8 @@ package com.lojasapato.lojasapato.infrastructure.repositories;
 import com.lojasapato.lojasapato.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+    Optional<Usuario> findUsuarioByEmail(String email);
 }
