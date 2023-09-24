e9bb3488926fdc8393db5c5abd738a9fbc8dc743
Luan Rafael <luanraffaell@hotmail.com>
2023-09-24 16:39:12 -0300

    feat: exception de entidade em uso

---
+++ Loja sapato/backend/src/main/java/com/lojasapato/lojasapato/domain/exception/EntidadeEmUsoException.java
@@ -0,0 +1,8 @@
+package com.lojasapato.lojasapato.domain.exception;
+
+public class EntidadeEmUsoException extends RuntimeException
+{
+    public EntidadeEmUsoException(String message) {
+        super(message);
+    }
+}
