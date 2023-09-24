0934f17631a03a3f6c53b61a63f4a19c1946b4ee
Luan Rafael <luanraffaell@hotmail.com>
2023-09-24 04:20:44 -0300

    refactor:inserido método findByNomeContaining em ProdutoRepository

--- Loja sapato/backend/src/main/java/com/lojasapato/lojasapato/infrastructure/repositories/ProdutoRepository.java
+++ Loja sapato/backend/src/main/java/com/lojasapato/lojasapato/infrastructure/repositories/ProdutoRepository.java
@@ -3,5 +3,9 @@ package com.lojasapato.lojasapato.infrastructure.repositories;
 import com.lojasapato.lojasapato.domain.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProdutoRepository extends JpaRepository<Produto,Long> {
+    Optional<List<Produto>> findByNomeContaining(String nome);
 }
