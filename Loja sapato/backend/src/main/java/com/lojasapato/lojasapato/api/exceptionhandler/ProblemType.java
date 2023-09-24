1ea8b8f960dfa160f41d17a3632bf19643424057
Luan Rafael <luanraffaell@hotmail.com>
2023-09-24 16:22:07 -0300

    feat: enum ProblemType para customização de erros

---
+++ Loja sapato/backend/src/main/java/com/lojasapato/lojasapato/api/exceptionhandler/ProblemType.java
@@ -0,0 +1,22 @@
+package com.lojasapato.lojasapato.api.exceptionhandler;
+
+import lombok.Getter;
+
+@Getter
+public enum ProblemType {
+    RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado","Recurso não encontrado"),
+    ENTIDADE_EM_USO("/entidade-em-uso","Entidade em uso"),
+    ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+    MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel","Mensagem incompreensivel"),
+    PARAMETRO_INVALIDO("/parametro-invalido","Parametro inválido"),
+    DADOS_INVALIDOS("/dados-invalidos","Dados inválidos"),
+    ERRO_DE_SISTEMA("/erro-de-sistema","Erro interno");
+
+    private String title;
+    private String uri;
+
+    private ProblemType(String path,String title) {
+        this.uri = "localhost:8080"+path;
+        this.title = title;
+    }
+}
