9723f117b27bfa61a43d78b0ec6cb367d3439b8c
Luan Rafael <luanraffaell@hotmail.com>
2023-09-24 02:28:03 -0300

    projeto criado

---
+++ Loja sapato/backend/src/main/java/com/lojasapato/lojasapato/LojasapatoApplication.java
@@ -0,0 +1,13 @@
+package com.lojasapato.lojasapato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojasapatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojasapatoApplication.class, args);
+	}
+
+}
---
+++ Loja sapato/backend/src/test/java/com/lojasapato/lojasapato/LojasapatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojasapato.lojasapato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojasapatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
