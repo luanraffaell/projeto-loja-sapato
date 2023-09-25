375ae421fc4681e3477de593b078450e854606c2
Luan Rafael <luanraffaell@hotmail.com>
2023-09-25 09:28:52 -0300

    refactor: alterado a resposta do metodo listarPedidos para PedidoResponseDTO

--- Loja sapato/backend/src/main/java/com/lojasapato/lojasapato/domain/service/PedidoService.java
+++ Loja sapato/backend/src/main/java/com/lojasapato/lojasapato/domain/service/PedidoService.java
@@ -1,18 +1,23 @@
 package com.lojasapato.lojasapato.domain.service;
 
+import com.lojasapato.lojasapato.api.model.PedidoResponseDTO;
 import com.lojasapato.lojasapato.domain.model.Pedido;
 import com.lojasapato.lojasapato.infrastructure.repositories.PedidoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class PedidoService {
     private final PedidoRepository pedidoRepository;
 
-    public List<Pedido>listarPedidos(){
-        return this.pedidoRepository.findAll();
+    public List<PedidoResponseDTO>listarPedidos(){
+        return this.pedidoRepository.findAll()
+                .stream()
+                .map(pedido -> new PedidoResponseDTO(pedido))
+                .collect(Collectors.toList());
     }
 }
