6d1ca4ed35dbe6c0aa8f789c4b046161f0e72d63
Luan Rafael <luanraffaell@hotmail.com>
2023-09-25 09:37:13 -0300

    feat:criada a classe pedido controller

---
+++ Loja sapato/backend/src/main/java/com/lojasapato/lojasapato/api/controller/PedidoController.java
@@ -0,0 +1,33 @@
+package com.lojasapato.lojasapato.api.controller;
+
+import com.lojasapato.lojasapato.api.model.PedidoRequestDTO;
+import com.lojasapato.lojasapato.api.model.PedidoResponseDTO;
+import com.lojasapato.lojasapato.domain.service.EmitirPedido;
+import com.lojasapato.lojasapato.domain.service.PedidoService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pedidos")
+@AllArgsConstructor
+public class PedidoController {
+    private final PedidoService pedidoService;
+    private final EmitirPedido emitirPedido;
+
+    @GetMapping
+    public ResponseEntity<List<PedidoResponseDTO>> listarPedidos(){
+        return ResponseEntity.ok().body(this.pedidoService.listarPedidos());
+    }
+    @PostMapping
+    public ResponseEntity<PedidoResponseDTO> emitirPedido(@RequestBody PedidoRequestDTO pedidoDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.emitirPedido.emitirPedido(pedidoDTO));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<PedidoResponseDTO> buscarPedidoPorId(@PathVariable Long id){
+        return ResponseEntity.ok().body(this.pedidoService.buscarOuFalhar(id));
+    }
+}
