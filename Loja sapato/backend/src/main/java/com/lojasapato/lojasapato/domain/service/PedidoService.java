b4b90f490c5ee74d15ca5ebcb5c56b35017ea58a
Luan Rafael <luanraffaell@hotmail.com>
2023-09-24 21:41:15 -0300

    feat: criado metodo para listar pedidos

--- Loja sapato/backend/src/main/java/com/lojasapato/lojasapato/domain/service/PedidoService.java
+++ Loja sapato/backend/src/main/java/com/lojasapato/lojasapato/domain/service/PedidoService.java
@@ -1,7 +1,18 @@
 package com.lojasapato.lojasapato.domain.service;
 
+import com.lojasapato.lojasapato.domain.model.Pedido;
+import com.lojasapato.lojasapato.infrastructure.repositories.PedidoRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class PedidoService {
+    private final PedidoRepository pedidoRepository;
+
+    public List<Pedido>listarPedidos(){
+        return this.pedidoRepository.findAll();
+    }
 }
